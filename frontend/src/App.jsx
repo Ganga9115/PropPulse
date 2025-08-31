24d108f74f0a91b3800bdb82e562eb1be83129e4
DEEPIKA A <145033159+DeepikaAnandhan2@users.noreply.github.com>
2025-08-31 21:58:29 +0530

    Contact Page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,31 @@
 import { Routes, Route } from "react-router-dom";
-import LandingPage from './components/LandingPage';
-import LoginPage from './components/LoginPage';
-import SignUpPage from './components/SignUpPage';
-import SelectUnitPage from './components/SelectUnitPage';
-import RequestForm from './components/RequestForm';
-import AppLayout from './components/AppLayout';
-import AgreementPage from './components/AgreementPage';
-import RequestSubmittedPage from './components/RequestSubmittedPage';
+import LandingPage from "./components/LandingPage";
+import LoginPage from "./components/LoginPage";
+import SignUpPage from "./components/SignUpPage";
+import SelectUnitPage from "./components/SelectUnitPage";
+import RequestForm from "./components/RequestForm";
+import AppLayout from "./components/AppLayout";
+import AgreementPage from "./components/AgreementPage";
+import RequestSubmittedPage from "./components/RequestSubmittedPage";
 import TenantDashboard from "./components/TenantDashboard";
 import MaintenanceView from "./components/MaintenanceView";
 import MaintenanceRequest from "./components/MaintenanceRequest";
-import Rent from "./components/Rent"; 
+import Rent from "./components/Rent";
+import UnitDetailsPage from "./components/UnitDetailsPage";
+import Contact from "./components/Contact"; // <- new page
+
 import "./index.css";
-import UnitDetailsPage from './components/UnitDetailsPage';
 
 export default function App() {
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
-      <Route element={<AppLayout />}>
+
+      {/* Protected/Layout Routes */}
+     
         <Route path="/select-unit" element={<SelectUnitPage />} />
         <Route path="/request-form" element={<RequestForm />} />
         <Route path="/agreement" element={<AgreementPage />} />
@@ -28,9 +33,10 @@ export default function App() {
         <Route path="/dashboard" element={<TenantDashboard />} />
         <Route path="/maintenance" element={<MaintenanceView />} />
         <Route path="/maintenance/new" element={<MaintenanceRequest />} />
-        <Route path="/rent" element={<Rent />} /> 
+        <Route path="/rent" element={<Rent />} />
         <Route path="/unit-details" element={<UnitDetailsPage />} />
-      </Route>
+        <Route path="/contact" element={<Contact/>} />
+    
     </Routes>
   );
 }
